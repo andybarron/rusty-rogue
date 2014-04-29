c4ff3060f76d53d9e999550fd51d68f127f83058
Andy Barron <andrewLbarron@gmail.com>
2014-04-29 13:12:54 -0400

    Far less hacky way to fix the window style, hah!

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,5 @@
 use rsfml::graphics::{RenderWindow};
-use rsfml::window::{VideoMode, ContextSettings, Close, Resize, event, keyboard};
+use rsfml::window::{VideoMode, ContextSettings, DefaultStyle, event, keyboard};
 use rsfml::window::keyboard::Key;
 use rsfml::window::event::Event;
 use rsfml::system::{Clock, Time, sleep, Vector2f};
@@ -13,15 +13,12 @@ use rsfml::audio;
 
 use util::get_rc_resource;
 
-use std::cast;
-
 pub fn launch(screen: ~Screen, title: &str, w: uint, h: uint) {
 
 	// init window
 	let setting: ContextSettings = ContextSettings::default();
 	let mut window: RenderWindow = RenderWindow::new(VideoMode::new_init(w, h, 32),
-			title, unsafe { cast::transmute(Close as u32 | Resize as u32) },
-			&setting).expect("Cannot create a new Render Window.");
+			title, DefaultStyle, &setting).expect("Cannot create a new Render Window.");
 	window.set_vertical_sync_enabled(true);
 	window.set_key_repeat_enabled(false);
 
